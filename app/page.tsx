b278643ab95cc1ed71d56fb05f8b7fad45141545
robinina17 <robinina17@gmail.com>
2025-09-27 16:08:51 +0700

    feat: Image
    for check blur seeting throtiling in inspect
    
    lazy loading diload saat mendekati gambar
    kalau eager loading akan diload di awal meskipun belum telrihat

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,28 @@
 import { Counter } from "@/app/counter";
+import Image from "next/image";
+import { gambarKucing2 } from "@/public/image";
 export default function Home() {
   return (
     <div>
       hello <Counter />
+      {/* <Image
+        height={100}
+        width={100}
+        alt="cat"
+        src="https://images.unsplash.com/photo-1495360010541-f48722b34f7d?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Y2F0fGVufDB8fDB8fHww"
+      /> */}
+      {/* <div className="relative"> */}
+      <div className="relative size-72 rounded-full border-2 overflow-hidden">
+        <Image
+          src={gambarKucing2}
+          alt="gamabar kucing"
+          fill
+          className="object-cover"
+          placeholder="blur"
+          // blurDataURL=""
+        />
+      </div>
+      {/* </div> */}
     </div>
   );
 }
---
+++ public/image/index.tsx
@@ -0,0 +1,4 @@
+import gambarKucing from "./kucing.jpg";
+import gambarKucing2 from "./kucing.jpg";
+
+export { gambarKucing, gambarKucing2 };
