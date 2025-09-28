35b7e0cc215fdd72d702778d18849d217eb73924
robinina17 <robinina17@gmail.com>
2025-09-28 21:06:21 +0700

    fix: balik ke sebelumnya tanpa stream data dan tanpa client component

--- app/header.tsx
+++ app/header.tsx
@@ -1,14 +1,12 @@
 import Link from "next/link";
 import { getSetting } from "./queries/getSetting";
 import { Nav } from "./nav";
-import { Suspense } from "react";
 export async function Header() {
-  const setting = getSetting();
+  const setting = await getSetting();
   return (
     <header>
-      <Suspense fallback="loading..">
-        <Nav settingPromise={setting} />
-      </Suspense>
+      <div className="text-2xl">{setting.siteName}</div>
+      <Nav />
     </header>
   );
 }
